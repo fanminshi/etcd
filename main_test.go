2d77cbab3f9c217568f54970ee4d5a8b06dd8b50
fanmin shi <fanmin.shi@coreos.com>
2016-12-07 15:59:54 -0800

    unit test coverage working

--- main_test.go
+++ main_test.go
@@ -1,7 +1,5 @@
 package main
 
-// need this file to generate test binary
-
 import (
 	"flag"
 	"testing"
@@ -10,12 +8,10 @@ import (
 var systemTest *bool
 
 func init() {
-	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
+	systemTest = flag.Bool("systemTest", false, "Set to true to instrument binary for code coverage")
 }
 
-// Test started when the test binary is started. Only calls main.
-func TestSystem(t *testing.T) {
-
+func TestMain(t *testing.T) {
 	if *systemTest {
 		main()
 	}
