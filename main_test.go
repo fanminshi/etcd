5437893a843a8ab8f1b47fb5a656a0f64ec3d701
fanmin shi <fanmin.shi@coreos.com>
2016-12-06 18:34:29 -0800

    full coverage for unit test

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+// need this file to generate test binary
+
+import (
+	"flag"
+	"testing"
+)
+
+var systemTest *bool
+
+func init() {
+	systemTest = flag.Bool("systemTest", false, "Set to true when running system tests")
+}
+
+// Test started when the test binary is started. Only calls main.
+func TestSystem(t *testing.T) {
+
+	if *systemTest {
+		main()
+	}
+}
